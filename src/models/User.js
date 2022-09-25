528c51a374050b373334dd98ddc7929cc6661cf3
Yung Cher Ho <ycho527@hotmail.com>
2022-09-25 15:29:00 +0100

    Completed basic authentication routes

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,5 +11,44 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// salt and hash password before being stored
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err,hash) => {
+            if (err) {
+                return next(err);
+
+            }
+            user.password = hash;
+            next();
+        })
+    });
+
+
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err);
+            }
+            if (!isMatch) {
+                return reject(false);
+            }
+            resolve(true);
+        });
+    });
+}
 
 mongoose.model('User', userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const User = mongoose.model('User');
 
 const router = express.Router();
 
+// Sign up route
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
     try {
@@ -19,4 +20,24 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+// Sign in route
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({ error: "Must provide email and password." })
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(422).send({ error: "Invalid password or email."});
+    }
+    try {
+        await user.comparePassword(password);
+        const secret_key = process.env.JWT_SECRET_KEY;
+        const token = jwt.sign({ userId: user._id}, secret_key)
+        res.send({ token });
+    } catch (err) {
+        return res.status(422).send({ error: 'Invalid password or email.'});
+    }
+});
 module.exports = router;
