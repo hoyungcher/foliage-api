a266a4625f3e4a79f2e0f5968cb83a53b4eedb0a
Yung Cher Ho <ycho527@hotmail.com>
2022-09-29 17:38:16 +0100

    Added GET location route

---
+++ src/routes/locationRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../middleware/requireAuth');
+
+const Report = mongoose.model('Report');
+const Location = mongoose.model('Location');
+const Phenomenon = mongoose.model('Phenomenon');
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get('/locations/:locationId', async(req, res) => {
+    const location = await Location
+        .find({ _id: req.params.locationId })
+    const reports = await Report
+        .find({ location: req.params.locationId })
+        .sort({timestamp: -1})
+        .limit(20)
+        .populate("phenomenon");
+    
+    
+    res.send({ location, reports });
+})
+
+module.exports = router;
